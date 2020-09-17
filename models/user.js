5dd579a2e9d17a3c3130bfa5b8dcbceafe64f5be
Kyle <kylespinaio@hotmail.com>
2020-09-16 22:52:21 -0400

    commiting to save any change

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, minlength: 6, required: true },
+
     
 })
 
